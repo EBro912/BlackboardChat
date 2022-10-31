84603bb3bece7630421e8bca36ed8f51d7faaaec
Ethan <ebrogames@gmail.com>
2022-10-31 01:00:26 -0400

    basic working microsoft tutorial

---
+++ Hubs/Chat.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlackboardChat.Hubs
+{
+    public class Chat : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
